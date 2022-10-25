154a2edf24cf99d41d0bebca9240479f802f05bc
Artem Ugryumov <55375066+artemyg0811@users.noreply.github.com>
2022-10-25 09:45:38 +0300

    Update application.php

--- src/application.php
+++ src/application.php
@@ -3,8 +3,8 @@
 $name = $_POST['user_name'];
 // $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
-$token = "5697590314:AAEWoEVJMZssNFb0ZCBTmoJRYsv6hEAkhzA"; // http://joxi.ru/v295MB7tzM6g72 - при создании бота в BotFather дается токен
-$chat_id = "-863273456"; /* https://api.telegram.org/bot5697590314:AAEWoEVJMZssNFb0ZCBTmoJRYsv6hEAkhzA/getUpdates, где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее. http://joxi.ru/p27DqBgcWqBVXA - где потом взять chat_id. Сначала бота нужно добавить в группу и отправить сообщение. id у бота начинается с минуса*/
+$token = ""; // http://joxi.ru/v295MB7tzM6g72 - при создании бота в BotFather дается токен
+$chat_id = ""; /* https://api.telegram.org/bot5697590314:AAEWoEVJMZssNFb0ZCBTmoJRYsv6hEAkhzA/getUpdates, где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее. http://joxi.ru/p27DqBgcWqBVXA - где потом взять chat_id. Сначала бота нужно добавить в группу и отправить сообщение. id у бота начинается с минуса*/
 $arr = array(
   'Имя пользователя: ' => $name,
 //   'Телефон: ' => $phone,
